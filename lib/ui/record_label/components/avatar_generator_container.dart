df68f1fbde2c5840b594857b72325ca94f38266f
Johannes Naylor <jonaylor89@gmail.com>
2023-08-25 15:23:03 +0200

    refactor'

---
+++ lib/ui/record_label/components/avatar_generator_container.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intheloopapp/domains/generation_bloc/generation_bloc.dart';
+import 'package:intheloopapp/domains/navigation_bloc/navigation_bloc.dart';
+import 'package:intheloopapp/domains/navigation_bloc/tapped_route.dart';
+import 'package:intheloopapp/ui/record_label/components/avatars_preview.dart';
+import 'package:intheloopapp/ui/record_label/cubit/subscribed_cubit.dart';
+import 'package:intheloopapp/utils/current_user_builder.dart';
+
+class AvatarGeneratorContainer extends StatelessWidget {
+  const AvatarGeneratorContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CurrentUserBuilder(
+      builder: (context, currentUser) {
+        return BlocBuilder<SubscribedCubit, SubscribedState>(
+          builder: (context, state) {
+            return Column(
+              children: [
+                const AvatarsPreview(),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4,
+                      horizontal: 32,
+                    ),
+                    child: CupertinoButton.filled(
+                      onPressed: currentUser.isOutOfCredits
+                          ? null
+                          : () {
+                              context.read<GenerationBloc>().add(
+                                    const ResetGeneration(),
+                                  );
+                              context.push(
+                                GenerateAvatarPage(),
+                              );
+                            },
+                      child: Text(
+                        'generate avatar',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          color: currentUser.isOutOfCredits
+                              ? Colors.grey
+                              : Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        );
+      },
+    );
+  }
+}
