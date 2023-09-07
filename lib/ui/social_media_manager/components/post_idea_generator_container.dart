8453fcde2bb51c173d9093dcb666f8b40cb15d7b
Johannes Naylor <jonaylor89@gmail.com>
2023-09-07 15:02:07 -0400

    title

--- lib/ui/social_media_manager/components/post_idea_generator_container.dart
+++ lib/ui/social_media_manager/components/post_idea_generator_container.dart
@@ -14,49 +14,47 @@ class PostIdeaGeneratorContainer extends StatelessWidget {
       builder: (context, state) {
         return Column(
           children: [
-            switch (state.postIdea) {
-              None() => const SizedBox.shrink(),
-              Some(:final value) => GestureDetector(
-                  onTap: () {
-                    Clipboard.setData(
-                      const ClipboardData(text: 'fake album name'),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Copied to Clipboard'),
-                      ),
-                    );
-                  },
+            GestureDetector(
+              onTap: () {
+                Clipboard.setData(
+                  const ClipboardData(text: 'fake album name'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Copied to Clipboard'),
+                  ),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 4,
+                  horizontal: 32,
+                ),
+                child: Card(
                   child: Padding(
                     padding: const EdgeInsets.symmetric(
-                      vertical: 4,
-                      horizontal: 32,
+                      vertical: 12,
+                      horizontal: 12,
                     ),
-                    child: Card(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 12,
-                          horizontal: 12,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Flexible(
-                              child: Text(
-                                value,
-                                textAlign: TextAlign.center,
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                ),
-                              ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Flexible(
+                          child: Text(
+                            state.postIdea,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
                 ),
-            },
+              ),
+            ),
             const SizedBox(height: 16),
             SizedBox(
               width: double.infinity,
@@ -72,13 +70,13 @@ class PostIdeaGeneratorContainer extends StatelessWidget {
                           .read<SocialMediaManagerCubit>()
                           .generatePostIdea(),
                   child: Text(
-                    'generate IG post idea',
+                    'show me the next idea!',
                     style: state.credits <= 0
-                    ? null
-                    : const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white,
-                    ),
+                        ? null
+                        : const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
                   ),
                 ),
               ),
