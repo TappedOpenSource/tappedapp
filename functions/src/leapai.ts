3ab95de8672984b1eb8084563fa854040cf669f0
Johannes Naylor <jonaylor89@gmail.com>
2023-09-22 16:49:51 +0200

    update cloud functions

--- functions/src/leapai.ts
+++ functions/src/leapai.ts
@@ -1,8 +1,68 @@
 
 import { Leap } from "@leap-ai/sdk";
 import { LeapInferenceSchema } from "@leap-ai/sdk/dist/types/schemas/Inference";
+import { info } from "firebase-functions/logger";
+import { HttpsError } from "firebase-functions/v1/auth";
 
 export const sd = {
+  createTrainingJob: async ({
+    leapApiKey,
+    imageUrls,
+    name,
+    type,
+    webhookUrl,
+  }: {
+    leapApiKey: string;
+    imageUrls: string[];
+    name: string;
+    type: string;
+    userId: string;
+    webhookUrl: string;
+  }): Promise<string> => {
+    const formData = new FormData();
+    imageUrls.forEach((image) => {
+      formData.append("imageSampleUrls", image);
+    });
+    
+    formData.append(
+      "webhookUrl",
+      webhookUrl,
+    );
+    
+    formData.append("name", name);
+    formData.append("subjectType", type);
+    formData.append("subjectKeyword", "@subject");
+    
+    const options = {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        Authorization: `Bearer ${leapApiKey}`,
+      },
+      body: formData,
+    };
+    const resp = await fetch(
+      "https://api.tryleap.ai/api/v2/images/models/new",
+      options
+    );
+    
+    const { status, statusText } = resp;
+    const body = (await resp.json()) as {
+          id: string;
+          imageSamples: string[];
+        };
+
+    info({ status, statusText, body });
+    
+    if (body.id === undefined) {
+      throw new HttpsError(
+        "internal",
+        "the model id is undefined",
+      );
+    }
+    
+    return body.id;
+  },
   createInferenceJob: async ({
     leapApiKey,
     modelId,
@@ -31,7 +91,6 @@ export const sd = {
 
     return { inferenceId: data.id };
   },
-
   getInferenceJob: async ({
     leapApiKey,
     inferenceId,
@@ -50,7 +109,6 @@ export const sd = {
 
     return { inferenceJob: data };
   },
-
   deleteInferenceJob: async ({
     leapApiKey,
     inferenceId,
