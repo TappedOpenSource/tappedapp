2b635129d6a7f1fae5b1302eb505d9e1fddbd149
Johannes Naylor <jonaylor89@gmail.com>
2024-01-23 00:09:53 -0500

    map view

--- lib/ui/search/discover_view.dart
+++ lib/ui/search/discover_view.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_marker_cluster/flutter_map_marker_cluster.dart';
 import 'package:intheloopapp/domains/models/location.dart';
 import 'package:intheloopapp/domains/models/option.dart';
+import 'package:intheloopapp/domains/models/user_model.dart';
 import 'package:intheloopapp/domains/navigation_bloc/navigation_bloc.dart';
 import 'package:intheloopapp/domains/navigation_bloc/tapped_route.dart';
 import 'package:intheloopapp/ui/search/components/venue_card.dart';
 import 'package:intheloopapp/ui/themes.dart';
 import 'package:intheloopapp/ui/user_avatar.dart';
+import 'package:intheloopapp/ui/user_card.dart';
 import 'package:intheloopapp/utils/bloc_utils.dart';
 import 'package:intheloopapp/utils/debouncer.dart';
 import 'package:latlong2/latlong.dart';
@@ -36,10 +39,78 @@ class DiscoverView extends StatelessWidget {
     );
   }
 
+  Widget _buildMapButton(
+    BuildContext context, {
+    required String heroTag,
+    required IconData icon,
+    required VoidCallback onPressed,
+  }) {
+    final theme = Theme.of(context);
+    return FloatingActionButton(
+      elevation: 2,
+      heroTag: heroTag,
+      highlightElevation: 3,
+      backgroundColor: theme.colorScheme.background,
+      mini: true,
+      onPressed: onPressed,
+      splashColor: Colors.transparent,
+      child: Icon(
+        icon,
+        color: theme.colorScheme.onBackground,
+      ),
+    );
+  }
+
+  Widget _userSlider(List<UserModel> users) {
+    return SizedBox(
+      height: 200,
+      child: ScrollSnapList(
+        onItemFocus: (index) {},
+        selectedItemAnchor: SelectedItemAnchor.START,
+        itemCount: users.length,
+        itemSize: 150 + (8 * 2),
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 8,
+            ),
+            child: UserCard(user: users[index]),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _venueSlider(List<UserModel> venues) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 8,
+      ),
+      child: SizedBox(
+        height: 200,
+        child: ScrollSnapList(
+          onItemFocus: (index) {},
+          selectedItemAnchor: SelectedItemAnchor.START,
+          itemCount: venues.length,
+          itemSize: 150 + (8 * 2),
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+              ),
+              child: VenueCard(venue: venues[index]),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final defaultLoc = Location.rva();
     final database = context.database;
+    final mapController = MapController();
     return FutureBuilder(
       future: database.getRichmondVenues(),
       builder: (context, snapshot) {
@@ -50,6 +121,7 @@ class DiscoverView extends StatelessWidget {
               return Stack(
                 children: [
                   FlutterMap(
+                    mapController: mapController,
                     options: MapOptions(
                       minZoom: 10,
                       maxZoom: 18,
@@ -70,7 +142,7 @@ class DiscoverView extends StatelessWidget {
                       MarkerClusterLayerWidget(
                         options: MarkerClusterLayerOptions(
                           maxClusterRadius: 40,
-                          size: Size(40, 40),
+                          size: const Size(40, 40),
                           polygonOptions: PolygonOptions(
                             borderColor: tappedAccent,
                             color: tappedAccent.withOpacity(0.5),
@@ -128,6 +200,36 @@ class DiscoverView extends StatelessWidget {
                       ),
                     ],
                   ),
+                  Positioned(
+                    bottom: 32 + 10,
+                    right: 10,
+                    child: Column(
+                      children: [
+                        _buildMapButton(
+                          context,
+                          icon: Icons.add,
+                          onPressed: () {
+                            mapController.move(
+                              mapController.camera.center,
+                              mapController.camera.zoom + 1,
+                            );
+                          },
+                          heroTag: 'zoom-in',
+                        ),
+                        _buildMapButton(
+                          context,
+                          icon: Icons.remove,
+                          onPressed: () {
+                            mapController.move(
+                              mapController.camera.center,
+                              mapController.camera.zoom - 1,
+                            );
+                          },
+                          heroTag: 'zoom-out',
+                        ),
+                      ],
+                    ),
+                  ),
                   SafeArea(
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
@@ -173,27 +275,139 @@ class DiscoverView extends StatelessWidget {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 8,
+                    _venueSlider(rvaVenues),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 8,
                       ),
-                      child: SizedBox(
-                        height: 200,
-                        child: ScrollSnapList(
-                          onItemFocus: (index) {},
-                          selectedItemAnchor: SelectedItemAnchor.START,
-                          itemCount: rvaVenues.length,
-                          itemSize: 150 + (8 * 2),
-                          itemBuilder: (context, index) {
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 8,
-                              ),
-                              child: VenueCard(venue: rvaVenues[index]),
-                            );
-                          },
+                      child: Text(
+                        'Top Bookers',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
+                    ),
+                    FutureBuilder(
+                      future: database.getBookerLeaders(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CupertinoActivityIndicator(),
+                          );
+                        }
+
+                        final bookerLeaders = snapshot.data ?? [];
+                        return _userSlider(bookerLeaders);
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 8,
+                      ),
+                      child: Text(
+                        'Top DC Venues',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    FutureBuilder(
+                      future: database.getDCVenues(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CupertinoActivityIndicator(),
+                          );
+                        }
+
+                        final dcVenues = snapshot.data ?? [];
+                        return _venueSlider(dcVenues);
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 8,
+                      ),
+                      child: Text(
+                        'Top NoVa Venues',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    FutureBuilder(
+                      future: database.getNovaVenues(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CupertinoActivityIndicator(),
+                          );
+                        }
+
+                        final novaVenues = snapshot.data ?? [];
+                        return _venueSlider(novaVenues);
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 8,
+                      ),
+                      child: Text(
+                        'Top Maryland Venues',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    FutureBuilder(
+                      future: database.getMarylandVenues(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CupertinoActivityIndicator(),
+                          );
+                        }
+
+                        final marylandVenues = snapshot.data ?? [];
+                        return _venueSlider(marylandVenues);
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 16,
+                        horizontal: 8,
+                      ),
+                      child: Text(
+                        'Top Performers',
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    FutureBuilder(
+                      future: database.getBookingLeaders(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CupertinoActivityIndicator(),
+                          );
+                        }
+
+                        final bookingLeaders = snapshot.data ?? [];
+                        return _userSlider(bookingLeaders);
+                      },
+                    ),
+                    const SizedBox(
+                      height: 100,
                     ),
                   ],
                 ),
