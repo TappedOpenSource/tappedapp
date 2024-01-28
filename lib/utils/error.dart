4ed43f8712d985929bcb34483bb73db38423505f
Johannes Naylor <jonaylor89@gmail.com>
2024-01-28 16:13:14 -0500

    yeehaw

--- lib/utils/error.dart
+++ lib/utils/error.dart
@@ -1,7 +1,7 @@
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:intheloopapp/utils/app_logger.dart';
+import 'package:stack_trace/stack_trace.dart';
 
 Future<void> configureError() async {
   FlutterError.onError = (errorDetails) {
@@ -31,4 +31,15 @@ Future<void> configureError() async {
     // while doing every day development.
     await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
   }
+}
+
+/// Rethrows [error] with a stacktrace that is the combination of [stackTrace]
+/// and [StackTrace.current].
+Never throwErrorWithCombinedStackTrace(Object error, StackTrace stackTrace) {
+  final chain = Chain([
+    Trace.current(),
+    ...Chain.forTrace(stackTrace).traces,
+  ]).foldFrames((frame) => frame.package == 'intheloopapp');
+
+  Error.throwWithStackTrace(error, chain.toTrace().vmTrace);
 }
