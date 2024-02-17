17d387f8fc48bb28670c444aae93e22bc30fee08
Johannes Naylor <jonaylor89@gmail.com>
2024-02-17 15:56:28 -0500

    styling on the ops feed page

--- lib/ui/opportunity_feed/components/opportunity_feed.dart
+++ lib/ui/opportunity_feed/components/opportunity_feed.dart
@@ -13,6 +13,94 @@ import 'package:intheloopapp/ui/themes.dart';
 class OpportunityFeed extends StatelessWidget {
   const OpportunityFeed({super.key});
 
+  Widget _buildEmptyFeed(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 12
+          ),
+          child: Container(
+            height: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 10,
+                  spreadRadius: 5,
+                ),
+              ],
+              image: const DecorationImage(
+                image: AssetImage('assets/classic_edm.gif'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 6,
+                    vertical: 12,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 12,
+                    ),
+                    decoration: const BoxDecoration(
+                      color: Colors.black,
+                    ),
+                    child: Column(
+                      children: [
+                        const Text(
+                          "YOU'RE OUT",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 32,
+                            fontFamily: 'Rubik One',
+                            fontWeight: FontWeight.w900,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const Text(
+                          "you've gone thru all the opportunities in your area!",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        CupertinoButton(
+                          color: Theme.of(context).colorScheme.primary,
+                          borderRadius: BorderRadius.circular(15),
+                          child: const Text(
+                            'want more?',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onPressed: () => context.push(
+                            PaywallPage(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<OpportunityFeedCubit, OpportunityFeedState>(
@@ -32,43 +120,7 @@ class OpportunityFeed extends StatelessWidget {
 
         if (state.opportunities.isEmpty ||
             state.curOp >= state.opportunities.length) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const SizedBox(width: double.infinity),
-                const LogoWave(
-                  height: 100,
-                  width: 100,
-                ),
-                const Text(
-                  "you've gone thru all the opportunities in your area!",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                CupertinoButton(
-                  onPressed: () {
-                    context.push(PaywallPage());
-                  },
-                  color: tappedAccent,
-                  child: const Text(
-                    'want more?',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          );
+          return _buildEmptyFeed(context);
         }
 
         final curOp = state.opportunities[state.curOp];
