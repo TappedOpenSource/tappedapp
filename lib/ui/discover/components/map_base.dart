f27a83540dd5d8330d777f4bc11ff1f4a232193a
Johannes Naylor <jonaylor89@gmail.com>
2024-02-22 18:25:46 -0500

    fix

--- lib/ui/discover/components/map_base.dart
+++ lib/ui/discover/components/map_base.dart
@@ -26,66 +26,67 @@ class MapBase extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<DiscoverCubit, DiscoverState>(
-      builder: (context, state) {
-        return FutureBuilder(
-          future: context.read<DiscoverCubit>().initLocation(),
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
+    return FutureBuilder(
+      future: context.read<DiscoverCubit>().initLocation(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
 
-            final (lat, lng) = snapshot.data ?? (Location.nyc.lat, Location.nyc.lng);
-            return FlutterMap(
-              mapController: mapController,
-              options: MapOptions(
-                // minZoom: 10,
-                maxZoom: 18,
-                initialCenter: LatLng(lat, lng),
-                onPositionChanged: (position, hasGesture) {
-                  context.read<DiscoverCubit>().onBoundsChange(
+        final (lat, lng) =
+            snapshot.data ?? (Location.nyc.lat, Location.nyc.lng);
+        return FlutterMap(
+          mapController: mapController,
+          options: MapOptions(
+            // minZoom: 10,
+            maxZoom: 18,
+            initialCenter: LatLng(lat, lng),
+            onPositionChanged: (position, hasGesture) {
+              context.read<DiscoverCubit>().onBoundsChange(
                     position.bounds,
                   );
-                },
-              ),
-              children: [
-                BlocBuilder<AppThemeCubit, bool>(
-                  builder: (context, isDark) {
-                    final theme = isDark ? mapboxDarkStyle : mapboxLightStyle;
-                    return TileLayer(
-                      urlTemplate:
+            },
+          ),
+          children: [
+            BlocBuilder<AppThemeCubit, bool>(
+              builder: (context, isDark) {
+                final theme = isDark ? mapboxDarkStyle : mapboxLightStyle;
+                return TileLayer(
+                  urlTemplate:
                       'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-                      additionalOptions: {
-                        'accessToken': defaultMapboxToken,
-                        'id': theme,
-                      },
-                      tileProvider: FMTC.instance('mapStore').getTileProvider(),
-                    );
+                  additionalOptions: {
+                    'accessToken': defaultMapboxToken,
+                    'id': theme,
                   },
-                ),
-                switch (state.mapOverlay) {
+                  tileProvider: FMTC.instance('mapStore').getTileProvider(),
+                );
+              },
+            ),
+            BlocBuilder<DiscoverCubit, DiscoverState>(
+              builder: (context, state) {
+                return switch (state.mapOverlay) {
                   MapOverlay.venues => const VenueMarkerLayer(),
                   MapOverlay.bookings => BookingsHeatmapLayer(),
                   MapOverlay.opportunities => const OpportunitiesHeatmapLayer(),
-                },
-                RichAttributionWidget(
-                  animationConfig: const ScaleRAWA(),
-                  // Or `FadeRAWA` as is default
-                  showFlutterMapAttribution: false,
-                  attributions: [
-                    TextSourceAttribution(
-                      'OpenStreetMap contributors',
-                      onTap: () => launchUrl(
-                        Uri.parse('https://openstreetmap.org/copyright'),
-                      ),
-                    ),
-                  ],
+                };
+              },
+            ),
+            RichAttributionWidget(
+              animationConfig: const ScaleRAWA(),
+              // Or `FadeRAWA` as is default
+              showFlutterMapAttribution: false,
+              attributions: [
+                TextSourceAttribution(
+                  'OpenStreetMap contributors',
+                  onTap: () => launchUrl(
+                    Uri.parse('https://openstreetmap.org/copyright'),
+                  ),
                 ),
               ],
-            );
-          },
+            ),
+          ],
         );
       },
     );
