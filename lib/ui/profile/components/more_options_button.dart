2aac7f17cc8afba49fc747033cfc535082f482c0
Johannes Naylor <jonaylor89@gmail.com>
2024-04-09 10:21:55 -0400

    verify

--- lib/ui/profile/components/more_options_button.dart
+++ lib/ui/profile/components/more_options_button.dart
@@ -29,155 +29,167 @@ class MoreOptionsButton extends StatelessWidget {
     final dateFormat = DateFormat('yyyy-MM-dd HH:mm:ss');
     showCupertinoModalPopup<void>(
       context: context,
-      builder: (BuildContext context) => CupertinoActionSheet(
-        title: Text(user.displayName),
-        // message: Text(user.username.username),
-        actions: <CupertinoActionSheetAction>[
-          if (isAdmin && user.id != currentUser.id)
-            switch (user.unclaimed) {
-              true => CupertinoActionSheetAction(
-                  onPressed: () {},
-                  child: const Text('unclaimed account'),
-                ),
-              false => CupertinoActionSheetAction(
+      builder: (BuildContext context) => FutureBuilder(
+        future: database.isVerified(user.id),
+        builder: (context, snapshot) {
+          final isVerified = snapshot.data ?? false;
+          return CupertinoActionSheet(
+            title: Text(user.displayName),
+            // message: Text(user.username.username),
+            actions: <CupertinoActionSheetAction>[
+              if (isAdmin && user.id != currentUser.id)
+                switch (user.unclaimed) {
+                  true => CupertinoActionSheetAction(
+                      onPressed: () {},
+                      child: const Text('unclaimed account'),
+                    ),
+                  false => CupertinoActionSheetAction(
+                      onPressed: () {
+                        // Copy to clipboard
+                        Clipboard.setData(
+                          ClipboardData(
+                            text: user.latestAppVersion.getOrElse(() => ''),
+                          ),
+                        ).then((value) {
+                          Navigator.pop(context);
+                          scaffoldMessenger.showSnackBar(
+                            const SnackBar(
+                              behavior: SnackBarBehavior.floating,
+                              backgroundColor: tappedAccent,
+                              content: Text(
+                                  'latest app version copied to clipboard'),
+                            ),
+                          );
+                        });
+                      },
+                      child: Text(
+                        'latest app version ${user.latestAppVersion.getOrElse(() => 'unknown')}',
+                      ),
+                    ),
+                },
+              if (isAdmin)
+                switch (user.timestamp) {
+                  None() => CupertinoActionSheetAction(
+                      onPressed: () {},
+                      child: const Text('joined unknown'),
+                    ),
+                  Some(:final value) => CupertinoActionSheetAction(
+                      onPressed: () {},
+                      child: Text('joined ${dateFormat.format(value)}'),
+                    ),
+                },
+              if (isAdmin)
+                CupertinoActionSheetAction(
                   onPressed: () {
                     // Copy to clipboard
                     Clipboard.setData(
-                      ClipboardData(
-                        text: user.latestAppVersion.getOrElse(() => ''),
-                      ),
+                      ClipboardData(text: user.id),
                     ).then((value) {
                       Navigator.pop(context);
                       scaffoldMessenger.showSnackBar(
                         const SnackBar(
                           behavior: SnackBarBehavior.floating,
                           backgroundColor: tappedAccent,
-                          content:
-                              Text('latest app version copied to clipboard'),
+                          content: Text('id copied to clipboard'),
                         ),
                       );
                     });
                   },
-                  child: Text(
-                    'latest app version ${user.latestAppVersion.getOrElse(() => 'unknown')}',
-                  ),
+                  child: Text('userId ${user.id}'),
                 ),
-            },
-          if (isAdmin)
-            switch (user.timestamp) {
-              None() => CupertinoActionSheetAction(
-                  onPressed: () {},
-                  child: const Text('joined unknown'),
-                ),
-              Some(:final value) => CupertinoActionSheetAction(
-                  onPressed: () {},
-                  child: Text('joined ${dateFormat.format(value)}'),
-                ),
-            },
-          if (isAdmin)
-            CupertinoActionSheetAction(
-              onPressed: () {
-                // Copy to clipboard
-                Clipboard.setData(
-                  ClipboardData(text: user.id),
-                ).then((value) {
-                  Navigator.pop(context);
-                  scaffoldMessenger.showSnackBar(
-                    const SnackBar(
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: tappedAccent,
-                      content: Text('id copied to clipboard'),
+              if (isAdmin)
+                switch (isVerified) {
+                  true => CupertinoActionSheetAction(
+                      onPressed: () {},
+                      child: const Text('✅ verified'),
                     ),
-                  );
-                });
-              },
-              child: Text('userId ${user.id}'),
-            ),
-          if (isAdmin)
-            CupertinoActionSheetAction(
-              onPressed: () {
-                database.verifyUser(user.id).then((value) {
-                  scaffoldMessenger.showSnackBar(
-                    const SnackBar(
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: tappedAccent,
-                      content: Text('user verified'),
+                  false => CupertinoActionSheetAction(
+                      onPressed: () {
+                        database.verifyUser(user.id).then((value) {
+                          scaffoldMessenger.showSnackBar(
+                            const SnackBar(
+                              behavior: SnackBarBehavior.floating,
+                              backgroundColor: tappedAccent,
+                              content: Text('user verified'),
+                            ),
+                          );
+                        });
+                      },
+                      child: Text('verify ${user.username}'),
                     ),
-                  );
-                });
-              },
-              child: Text('verify ${user.username}'),
-            ),
-          CupertinoActionSheetAction(
-            onPressed: () {
-              FirebaseAnalytics.instance.logEvent(
-                name: 'share_profile',
-                parameters: {
-                  'user_id': user.id,
-                  'type': 'link',
                 },
-              );
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  FirebaseAnalytics.instance.logEvent(
+                    name: 'share_profile',
+                    parameters: {
+                      'user_id': user.id,
+                      'type': 'link',
+                    },
+                  );
 
-              Share.share('https://tapped.ai/${user.username}')
-                  .onError((error, stackTrace) {
-                logger.error(
-                  'Error sharing profile',
-                  error: error,
-                  stackTrace: stackTrace,
-                );
-                scaffoldMessenger.showSnackBar(
-                  const SnackBar(
-                    behavior: SnackBarBehavior.floating,
-                    backgroundColor: Colors.red,
-                    content: Text('Error sharing profile'),
-                  ),
-                );
-              });
-            },
-            child: const Text('share profile'),
-          ),
-          if (user.id != currentUser.id)
-            CupertinoActionSheetAction(
-              onPressed: () {
-                nav.pop();
-                database
-                    .reportUser(
-                  reported: user,
-                  reporter: currentUser,
-                )
-                    .then((value) {
-                  scaffoldMessenger.showSnackBar(
-                    const SnackBar(
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: tappedAccent,
-                      content: Text('User Reported'),
-                    ),
-                  );
-                });
-              },
-              child: const Text('report user'),
-            ),
-          if (user.id != currentUser.id)
-            CupertinoActionSheetAction(
-              /// This parameter indicates the action would perform
-              /// a destructive action such as delete or exit and turns
-              /// the action's text color to red.
-              isDestructiveAction: true,
-              onPressed: () {
-                nav.pop();
-                context.read<ProfileCubit>().block().then((value) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: Colors.red,
-                      content: Text('User Blocked'),
-                    ),
-                  );
-                });
-              },
-              child: const Text('block user'),
-            ),
-        ],
+                  Share.share('https://tapped.ai/${user.username}')
+                      .onError((error, stackTrace) {
+                    logger.error(
+                      'Error sharing profile',
+                      error: error,
+                      stackTrace: stackTrace,
+                    );
+                    scaffoldMessenger.showSnackBar(
+                      const SnackBar(
+                        behavior: SnackBarBehavior.floating,
+                        backgroundColor: Colors.red,
+                        content: Text('Error sharing profile'),
+                      ),
+                    );
+                  });
+                },
+                child: const Text('share profile'),
+              ),
+              if (user.id != currentUser.id)
+                CupertinoActionSheetAction(
+                  onPressed: () {
+                    nav.pop();
+                    database
+                        .reportUser(
+                      reported: user,
+                      reporter: currentUser,
+                    )
+                        .then((value) {
+                      scaffoldMessenger.showSnackBar(
+                        const SnackBar(
+                          behavior: SnackBarBehavior.floating,
+                          backgroundColor: tappedAccent,
+                          content: Text('User Reported'),
+                        ),
+                      );
+                    });
+                  },
+                  child: const Text('report user'),
+                ),
+              if (user.id != currentUser.id)
+                CupertinoActionSheetAction(
+                  /// This parameter indicates the action would perform
+                  /// a destructive action such as delete or exit and turns
+                  /// the action's text color to red.
+                  isDestructiveAction: true,
+                  onPressed: () {
+                    nav.pop();
+                    context.read<ProfileCubit>().block().then((value) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          behavior: SnackBarBehavior.floating,
+                          backgroundColor: Colors.red,
+                          content: Text('User Blocked'),
+                        ),
+                      );
+                    });
+                  },
+                  child: const Text('block user'),
+                ),
+            ],
+          );
+        },
       ),
     );
   }
