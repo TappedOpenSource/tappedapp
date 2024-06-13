cd7e627be0db1a0fb36065241549416efed5c5a5
Johannes Naylor <jonaylor89@gmail.com>
2024-06-13 13:57:55 -0400

    fix wording

--- functions/src/email_templates/subscription_expiration.ts
+++ functions/src/email_templates/subscription_expiration.ts
@@ -4,9 +4,12 @@ hey,
 
 your Tapped Premium subscription is expiring :(
 
-you can renew your subscriptionn in the app or on our website at https://app.tapped.ai/subscribe
+you can renew your subscription in the app or on our website at https://app
+.tapped.ai/premium
 
-if you have any questions or feedback, pls email me (johannes@tapped.ai) cause i'd love to know how we can make the product better
+if you have any questions or feedback, pls email me or ilias
+(johannes@tapped.ai or ilias@tapped.ai) cause i'd love to know how we can
+make the product better
 
 best,
 Johannes
